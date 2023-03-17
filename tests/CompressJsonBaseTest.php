cebc1756df2c3d0e567384d102d6a7f92fe927bf
Islam Zaripov <islam.zar@yandex.ru>
2023-03-17 17:02:33 +0300

    Add PHPUnit test

---
+++ tests/CompressJsonBaseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use CompressJson\Core\Compressor;
+use PHPUnit\Framework\TestCase;
+
+class CompressJsonBaseTest extends TestCase
+{
+    public function testSimpleData()
+    {
+        ini_set('precision', 16);
+        $longStr = 'A very very long string, that is repeated';
+        $data = [
+            'int' => 42,
+            'float' => 12.34,
+            'str' => 'Alice',
+            'longStr',
+            'longNum' => 9876543210.123455,
+            'bool' => true,
+            'bool2' => false,
+            'arr' => [42, $longStr],
+            'arr2' => [42, $longStr], // identical values will be deduplidated, including array and object
+            'obj' => [ // nested values are supported
+                'id' => 123,
+                'name' => 'Alice',
+                'role' => ['Admin', 'User', 'Guest'],
+                'longStr' => 'A very very long string, that is repeated',
+                'longNum' => 9876543210.123455
+            ],
+            'escape' => ['s|str', 'n|123', 'o|1', 'a|1', 'b|T', 'b|F']
+        ];
+        $compressed = Compressor::create()
+            ->compress($data);
+        $compressedJson = $compressed
+            ->toJson();
+        $decompressed = Compressor::create()
+            ->decompressJson($compressedJson);
+        // Assert
+        $this->assertInstanceOf('CompressJson\Core\Compressed', $compressed);
+        $this->assertIsString($compressedJson);
+        $this->assertEquals($data, $decompressed);
+    }
+}
